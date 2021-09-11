0bc3848cf143df12312eed769a122fe986b1ea0a
Qizhen-Yang <qizhen_yang@163.com>
2021-09-11 16:01:06 +0800

    Update dictionaree.py

--- dictionaree.py
+++ dictionaree.py
@@ -15,22 +15,24 @@ header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
          "Cookie": "your cookie"}
 d = []
 for word in i:
-    result = requests.get("https://www.bing.com/dict/search?q=" + word, headers=header)
+    result = requests.get("http://youdao.com/w/eng/" + word, headers=header)
     result_bs = BeautifulSoup(result.text, "html.parser")
-    newitem = result_bs.find_all("div", attrs = {'class': 'hd_area'}) + result_bs.find_all("span", attrs = {'class': 'def b_regtxt'})
-    if len(newitem) < 2:
+    newitem = str(result_bs.find("h2", attrs = {'class': 'wordbook-js'}))
+    if newitem == "None":
         d.append([word, "<br/> No definitions"])
         print("Failed to find definition of:", word)
     else:
+        newitem += str(result_bs.find("div", attrs = {'class': 'trans-container'}))
         d.append(newitem)
         print("Added word:", word)
 
 f = open("Dictionaree.html", "w+", encoding = "utf-8")
-f.write("<meta charset='utf-8'/><style>*{font-size:16px;line-height:36px;}html{margin:auto;text-align:center;max-width:21cm;}hr{width:50%;}</style>")
+f.write("<meta charset='utf-8'/><style>*{font-size:16px;}.word{margin:auto;width:fit-content;text-align:center;}.baav{font-weight:400;margin-top:16px;}html{margin:auto;max-width:21cm;}hr{width:50%;}.additional{display:none;}</style>")
 for word_def in d:
+    f.write("<div class='word'>")
     for j in word_def:
         f.write(str(j))
-    f.write("<hr/>")
+    f.write("</div><hr/>")
 f.close()
 print("Generated file: Dictionaree.html")
 os.startfile("Dictionaree.html")
