ddfcbd08bcc78d6008a597284e713298c3ac97a0
Qizhen-Yang <qizhen_yang@163.com>
2021-12-18 12:12:11 +0800

    调整了Collins词典的来源 修复了输入问题

--- collins.py
+++ collins.py
@@ -2,17 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 def search(word):
-	header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Cookie": "your cookie"}
-	result = requests.get("https://www.collinsdictionary.com/dictionary/english/" + word, headers=header)
+	header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Cookie": "Collins"}
+	result = requests.get("http://youdao.com/w/eng/" + word, headers=header)
 	result_bs = BeautifulSoup(result.text, "html.parser")
-	newitem = str(result_bs.find("h2", attrs = {'class': 'h2_entry'}))
+	newitem = str(result_bs.find("span", attrs = {"class": "title"}))
 	if newitem == "None":
-		d = ["<div class='nodef'><strong>", word, "</strong><br/><br/> No definitions </div>"]
-		print("Failed to find definition of:", word)
+		d = "<p>[Collins] No definitions</p>"
+		print("[Collins]\tNo def of:\t", word)
 	else:
-		newitem = str(result_bs.find("div", attrs = {'class': 'dictentry'}))
+		newitem = str(result_bs.find("div", attrs = {'id': 'authTrans'}))
+		newitem = newitem.replace("<ul", "<div")
+		newitem = newitem.replace("</ul>", "</div>")
+		newitem = newitem.replace("<li>", "<p>")
+		newitem = newitem.replace("</li>", "</p>")
 		newitem = newitem.replace("<a", "<span")
 		newitem = newitem.replace("</a>", "</span>")
 		d = newitem
-		print("[Collins]", "Added word:", word)
+		print("[Collins]\tAdded word:\t", word)
 	return d
--- youdao.py
+++ youdao.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 def search(word):
-	header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Cookie": "your cookie"}
+	header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Cookie": "Youdao"}
 	result = requests.get("http://youdao.com/w/eng/" + word, headers=header)
 	result_bs = BeautifulSoup(result.text, "html.parser")
 	newitem = str(result_bs.find("h2", attrs = {'class': 'wordbook-js'}))
 	if newitem == "None":
-		d = ["<div class='nodef'><strong>", word, "</strong><br/><br/> No definitions </div>"]
-		print("Failed to find definition of:", word)
+		d = "<p>[Youdao] No definitions</p>"
+		print("[Youdao]\tNo def of:\t", word)
 	else:
 		newitem += str(result_bs.find("div", attrs = {'class': 'trans-container'}))
 		newitem = newitem.replace("<ul>", "<div>")
@@ -18,5 +18,5 @@ def search(word):
 		newitem = newitem.replace("<a", "<span")
 		newitem = newitem.replace("</a>", "</span>")
 		d = newitem
-		print("[Youdao]", "Added word:", word)
+		print("[Youdao]\tAdded word:\t", word)
 	return d
